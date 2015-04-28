8304405f0aae754a42df507146c9efb84c72dcbb
metasyn <alexx.johnson@gmail.com>
2015-04-27 21:52:51 -0700

    move plot to ui.js

--- js/ui.js
+++ js/ui.js
@@ -2,3 +2,13 @@
 $('.content.icon').click(function(){
     $('.ui.sidebar').sidebar('toggle');
   });
+
+// add loaders
+
+function plot(){
+  setTimeout(function(){
+    plotData();
+  $('#loader').removeClass('ui active dimmer');
+  }, 1);
+  $('#loader').addClass('ui active dimmer');
+}
